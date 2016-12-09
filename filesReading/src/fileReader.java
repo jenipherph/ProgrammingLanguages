d7a87b68e7be8a01c2830099cb49db7082a902d3
Jenipher Gonzalez <gonzalezjenipher649@gmail.com>
2016-12-09 16:21:38 -0400

    Editing of fileReader.

--- filesReading/src/fileReader.java
+++ filesReading/src/fileReader.java
@@ -10,8 +10,161 @@
  *
  */
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Objects;
+import java.util.Scanner;
+
 public class fileReader {
+
+   // private static final String FILENAME = "/Users/jeniphergonzalez/Documents/UT/ProgrammingLanguages/" +
+     //       "resources/examples/example1.java";
     public static void main(String [] args){
 
+        Scanner scanner = null;
+       // Scanner scannerLine = null;
+        String []  stringArray= null;
+
+        String delim = " ";
+
+        int count = 0;
+
+        try {
+            scanner = new Scanner(new BufferedReader(new FileReader(menu())));
+           // scannerLine = new Scanner(stringArrayLine[count]);
+            if(scanner != null)
+                //stringArrayLine = new String[500];
+                stringArray = new String[500];
+
+            while(scanner.hasNext()){
+                //stringArrayLine[count] = scanner.nextLine().trim().toString();
+               // System.out.println(count + ". " + stringArrayLine[count]);
+
+                stringArray[count] = scanner.next().trim().toString();
+                System.out.println(count + ". " + stringArray[count]);
+
+                count++;
+            }
+
+            for(int i = 0; i < stringArray.length; i++)
+                switch (stringArray[i]) {
+                    case "import":
+                        importDeclaration(stringArray[++i]);
+                        System.out.print("This is a keyword: " + stringArray[i]+"\n");
+                        System.out.println("You have an import! Line " + i);
+                        i++;
+                        System.out.println("You must have: java.*;");
+                        System.out.println("You have: " + stringArray[i]);
+                       if (Objects.equals(stringArray[i], "java.*;") && Objects.equals(stringArray[++i], "class"))
+                           System.out.println("true!");
+                        else
+                        System.out.println("Error! Line: " + i);
+                        break;
+                    case "class":
+                        System.out.println("This is a keyword: " + stringArray[i]);
+                        System.out.println("You have a class! Line " + i);
+                        if(Objects.equals(stringArray[i], "{"))
+                            System.out.println("We're good!");
+                        break;
+                    case "public":
+                        System.out.println("This is a keyword: " + stringArray[i]);
+                        System.out.println("You have a public! Line " + i);
+                        break;
+                    case "protected":
+                        System.out.print("This is a keyword: " + stringArray[i]+"\n");
+                        System.out.println("You have an protected! Line " + i);
+                        break;
+                    case "private":
+                        System.out.println("This is a keyword: " + stringArray[i]);
+                        System.out.println("You have a private! Line " + i);
+                        break;
+                    case "static":
+                        System.out.println("This is a keyword: " + stringArray[i]);
+                        System.out.println("You have a static! Line " + i);
+                        break;
+                    case "final":
+                        System.out.println("This is a keyword: " + stringArray[i]);
+                        System.out.println("You have a final! Line " + i);
+                        break;
+                    case "transient":
+                        System.out.println("This is a keyword: " + stringArray[i]);
+                        System.out.println("You have a transient! Line " + i);
+                        break;
+                    case "volatile":
+                        System.out.println("This is a keyword: " + stringArray[i]);
+                        System.out.println("You have a volatile! Line " + i);
+                        break;
+                    case "synchronized":
+                        System.out.println("This is a keyword: " + stringArray[i]);
+                        System.out.println("You have a synchronized! Line " + i);
+                        break;
+                    case "void":
+                        System.out.println("This is a keyword: " + stringArray[i]);
+                        System.out.println("You have a void! Line " + i);
+                        break;
+                    //Variables Keywords: int and double
+                    case "int":
+                        System.out.println("This is a keyword: " + stringArray[i]);
+                        System.out.println("You have a int! Line " + i);
+                        break;
+                    case "double":
+                        System.out.println("This is a keyword: " + stringArray[i]);
+                        System.out.println("You have a double! Line " + i);
+                        break;
+                    default:
+                        System.out.println("Check for another function! Line: " + i);
+                        break;
+                }
+
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            if(scanner != null)
+                scanner.close();
+        }
+    }
+    public static String menu(){
+        String [] codeName = new String[7];
+        String fileName;
+        int value;
+
+        Scanner input = new Scanner(System.in);
+
+        codeName[0] = "example1.java";
+        codeName[1] = "example2.java";
+        codeName[2] = "example3.java";
+        codeName[3] = "example4.java";
+        codeName[4] = "example5.java";
+        codeName[5] = "example6.java";
+        codeName[6] = "example7.java";
+
+        System.out.println("Mini-Java");
+        System.out.println("Select Examples (1) or Errors! (2). Insert the #");
+        value = input.nextInt();
+
+        if(Objects.equals(value,  1))
+            fileName = "/Users/jeniphergonzalez/Documents/UT/ProgrammingLanguages/resources/examples/";
+        else
+            fileName = "/Users/jeniphergonzalez/Documents/UT/ProgrammingLanguages/resources/errors/";
+
+        System.out.println("Select a file:");
+        for(int i = 0; i < codeName.length; i++)
+            System.out.println(i + ". " + codeName[i]);
+
+        System.out.print("Select a File name: ");
+        input.nextLine();
+       fileName +=  input.nextLine();
+
+        System.out.println(fileName );
+        return fileName;
+    }
+
+    public static void importDeclaration(String term){
+        if (Objects.equals(term, "java.*;"))
+            System.out.println("We're good");
+        else
+            System.out.println("Error at Import Declaration " + term);
     }
 }
